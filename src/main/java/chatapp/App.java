a9f8ac705d330433eeaea0bfa4821cce6f39d0c8
hlam3004 <hlam.30042005@gmail.com>
2025-10-30 20:35:26 +0700

    init commit

--- src/main/java/chatapp/App.java
+++ src/main/java/chatapp/App.java
@@ -4,7 +4,6 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -15,18 +14,11 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        scene = new Scene( new Label("haha"),640, 480);
+        Parent root = FXMLLoader.load(getClass().getResource("dashboard_user.fxml"));
+        stage.setTitle("TEST");
+        scene = new Scene(root);
         stage.setScene(scene);
         stage.show();
-    }
-
-    static void setRoot(String fxml) throws IOException {
-        scene.setRoot(loadFXML(fxml));
-    }
-
-    private static Parent loadFXML(String fxml) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource(fxml + ".fxml"));
-        return fxmlLoader.load();
     }
 
     public static void main(String[] args) {
